308aff1fb597735aa56a9ef62a3800b08dd78f85
Karnaj <yoan.geran@hotmail.fr>
2019-01-05 16:49:21 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,53 +1,19 @@
-from functools import total_ordering
 import matplotlib.pyplot as plt
-from random import randint
+import data_structures
+from convex_hull import compute_convex_hull_direct
 
-@total_ordering
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, other):
-        self.x == other.x and self.y == other.y
-
-    def __lt__(self, other):
-        self.x < other.x or (self.x == other.x and self.y < other.y)
-
-    def is_external(self, a, b):
-        return (b.x-a.x)*(self.y-a.y)-(b.y-a.y)*(self.x-a.x)>0
-
-    def __str__(self):
-        return "({}, {})".format(self.x, self.y)
-
-def is_hull_side(p, q, points):
-    if p == q:
-        return False
-    return all(not point.is_external(p, q) for point in points)
-
-def compute_convex_hull(points):
-    N = len(points)
-    hull = []
-    for i in range(N):
-        for j in range(N):
-            if i != j:
-                if is_hull_side(points[i], points[j], points):
-                    hull.append([points[i], points[j]])
-    return hull
-
-def print_points(points):
+def print_points(points, color='black', s=10)
     X = [p.x for p in points]
     Y = [p.y for p in points]
-    plt.plot(X, Y)
+    plt.scatter(X, Y, s=10, color='black')
 
+def print_segments(segments, color='red', lw=1):
+    for s in segments:
+        plt.plot([s.p1.x, s.p2x], [s.p1.y, s.p2.y], color=color, lw=lw)
+        
 points = [Point(randint(0, 10000000000), randint(0, 10000000000)) for i in range(2019)]
-X = [p.x for p in points]
-Y = [p.y for p in points]
-plt.scatter(X, Y, s=10, color='black')
-convex_hull = compute_convex_hull(points)
-print(len(convex_hull))
-#[print(s[0], s[1]) for s in convex_hull]
-for s in convex_hull:
-    plt.plot([s[0].x, s[1].x], [s[0].y, s[1].y], color='red', lw=1)
+convex_hull = compute_convex_hull_direct(points)
+print_points(points)
+print_segments(convex_hull)
 plt.show()
 plt.close()
