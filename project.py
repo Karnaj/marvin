d46eb627b6aa021f671a8275d6881d45c34ead45
Karnaj <yoan.geran@hotmail.fr>
2019-01-19 12:34:59 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from data_structures import *
 from convex_hull import *
-from minkowski_sum import *
-from random import randint
+from triangulation import *
+from random import randint, sample
 import itertools
+from visibility_graph import *
 
 def print_points(points, color='black', s=10):
     X = [p.x for p in points]
@@ -14,11 +15,7 @@ def print_segments(segments, color='blue', lw=1):
     for s in segments:
         plt.plot([s.p1.x, s.p2.x], [s.p1.y, s.p2.y], color=color, lw=lw)
 
-def convex_minkowski_sum(A, B):
-    p = min(A.points)
-    points = [Point(p1.x + p2.x - p.x, p1.y + p2.y - p.y) for (p1, p2) in itertools.product(A.points, B.points)]
-    return compute_convex_hull_incremental(points)
-    
+
 def visibility_graph(obstacle, p1, p2):
     N = len(obstacle) + 2
     graph = [[0 for i in range(N)] for j in range(N)]
@@ -36,50 +33,8 @@ def visibility_graph(obstacle, p1, p2):
             graph[i][N - 1] = 1  
     return graph
 
-def visibility_graph_c(polygon, segments): # possible only if no intersection into polygons
-    edges = [s for s in segments]
-    N = len(polygon)
-    for i in range(N):
-        for j in range(i + 1, N):
-            for p1 in polygon[i]:
-                for p2 in polygon[j]:
-                    s = Segment(p1, p2)
-                    if not any(s.has_strict_intersection(s1) for s1 in segments):
-                        print(s)
-                        edges.append(s)
-    return edges
-
-def visibility_graph_b(obstacles):
-    points = []
-    for (i, o) in enumerate(obstacles):
-        for p in o.points:
-            points.append((i, p))
-    points.sort(key=lambda x : x[1])
-    #segments = [[] for i in range(len(points))]
-    segments = []
-    edges = []
-    vus = []
-    for (i, p) in points:
-        segments.append(obstacles[i].edges_with(p)[1])
-        segments.append(obstacles[i].edges_with(p)[0])
-        for (j, p1) in vus:
-            s = Segment(p, p1)
-            #valide = True
-            #for i in range(p1.id, p.id):
-            #    if not any(s.has_strict_intersection(s1) for s1 in segments[i]):
-            #        valide = False
-            #        break
-            #print(valide)
-            #if valide:
-            if i != j or obstacles[i].external_segment(p, p1):
-                if not any(s.has_strict_intersection(s1) for s1 in segments):
-                    edges.append(s)
-        vus.append((i, p))
-    return edges    
-
-
 points_1 = [Point(randint(0, 40), randint(0, 50)) for i in range(10)]
-polygon_1 = compute_convex_hull_incremental(points_1)
+polygon_1 = Polygon.compute_convex_hull_incremental(points_1)
 
 polygon_2 = Polygon([Point(120, 120), Point(160, 120), Point(160, 80),
                           Point(120, 80), Point(120, 100), Point(140, 100),
@@ -88,7 +43,9 @@ polygon_2 = Polygon([Point(120, 120), Point(160, 120), Point(160, 80),
 
 
 points_3 = [Point(randint(0, 40), randint(90, 130)) for i in range(10)]
-polygon_3 = compute_convex_hull_incremental(points_3)
+polygon_3 = Polygon.compute_convex_hull_incremental(points_3)
+
+polygon_list = polygon_2.simple_triangulation()
 
 v = VisibilityGraph([polygon_1, polygon_2, polygon_3], [Point(70, 3), Point(15, 30)])
 
@@ -104,15 +61,26 @@ print_points(polygon_2.points())
 print_points(polygon_3.points())
 print_points(v.get_points())
 
+
+polygon_1 = Polygon([Point(0, 0), Point(0, 7), Point(7, 7), Point(7, 0)])
+polygon_list = [p.convex_minkowski_sum(polygon_1) for p in polygon_list]
+
+for p in polygon_list:
+    print_segments(p.segments(), color=sample(['b', 'g', 'r', 'c', 'm', 'y', 'k'], 1)[0], lw=1)
+    for p1 in p.points():
+        print(p1)    
+    print()
+    print()
+    
+v = VisibilityGraph(polygon_list, [Point(0, 7), Point(148, 100)])
+    
 print_segments(polygon_1.segments(), color='black', lw=3)
-print_segments(polygon_2.segments(), color='black', lw=3)
+print_segments(polygon_2.segments(), color='black', lw=2)
 print_segments(polygon_3.segments(), color='black', lw=3)
+
+
 print_segments(v.edges, color='red', lw=1)
 
 plt.show()
 plt.close()
-                
-                
-                
-                
-                
+   
