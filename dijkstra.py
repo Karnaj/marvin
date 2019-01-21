35ccaa995a25f12c72263596f9b8ae65b1621493
Lostindark <valentin.maestracci@gmail.com>
2019-01-22 00:31:41 +0100

    Dijkstra done.

--- dijkstra.py
+++ dijkstra.py
@@ -1,41 +1,103 @@
 
 
-# Take the graph as an adjacency list related to nodes (ie a 0 represent nodes[0])
-# n the length of nodes.
-# deb the index of the first node.
+class Heap:
+    def __init__(self, objs):
+        self.n = 0
+        self.heap = [None] #index starts at 1
+        self.rank = {}
+        for e in objs:
+            self.push(e)
 
-inf = float('inf')
+    def __len__(self):
+        return len(self.heap) - 1
 
-def dijkstra(graph, n, deb, fin):
+    def push(self, x):
+        assert x not in self.rank
+        i = len(self.heap)
+        self.heap.append(x)
+        self.rank[x] = i
+        self.up(i)
 
-    d = [inf for e in range(len(nodes))]
-    d[deb] = 0
+    def pop(self):
+        root = self.heap[1]
+        del self.rank[root]
+        x = self.heap.pop()
+        if self:
+            self.heap[1] = x
+            self.rank[x] = 1
+            self.down(x)
+        return root
 
-    pred = [-1 for e in range(len(nodes))]
-    pred[deb] = deb
+    def up(self, i):
+        x = self.heap[i]
+        while i > 1 and x < self.heap[i // 2]
+            self.heap[i] = self.heap[i // 2]
+            self.rank[self.heap[i // 2]] = i
+            i //= 2
+        self.heap[i] = x
+        self.rank[x] = i
 
-    # q
-    q = priority_queue()
-    while not q.empty():
+    def down(self, i):
+        x = self.heap[i]
+        n = len(self.heap[i])
+        while True:
+            left = 2*i
+            right = left + 1
+            if right < n and \
+               self.heap[right] < x and self.heap[right] < self.heap[left]:
 
-        s1 = q.pop()
-        for s2 in graph[s1]:
+                self.heap[i] = self.heap[right]
+                self.rank[self.heap[right]] = i
+                i = right
 
-            if d[s2] > d[s1] + w(s1, s2):
-                d[s2] = d[s1] + w(s1, s2)
-                pred[s2] = s1
+            elif left < n and self.heap[left] < x:
+                self.heap[i] = self.heap[left]
+                self.rank[self.heap[left]] = i
+                i = left
+            else:
+                self.heap[i] = x
+                self.rank[x] = i
+                return
 
-    return (pred, d)
+        def update(self, old, new):
+            i = self.rank[old]
+            del self.rank[old]
+            self.heap[i] = new
+            self.rank[new] = i
+            if old < new:
+                self.down(i)
+            else:
+                self.up(i)
 
-#Compute a path from beginning to end using distances computed in d.
-# beg must be the one used to compute d !!!
-def path(nodes, pred, end):
 
-    s = end
-    L = []
-    while s != pred[s]:
-        L.append(s)
-        s = pred[s]
+#####  Dijkstra using my heap as a priority queue.
+def dijkstra_update_heap(graph, weight, src = 0, trg = None):
+    n = len(graph)
+    prec = [None]*n
+    prec[src] = src
+    dist = [float('inf')] * n
+    dist[src] = 0
+    heap = Heap([(dist[node], node) for node in range(n)])
 
-    L.append(s)
+    while heap:
+        d_node, node = heap.pop()
+        if node == target:
+            break;
+
+        for nb in graph[node]:
+            old = dist[nb]
+            new = d_node + weight[node][nb]
+            if new < old:
+                dist[nb] = new
+                prec[nb] = node
+                heap.update((old, nb), (new, nb))
+
+    return dist, prec
+
+def find_path(prec, trg):
+    L = [trg]
+    while prec[trg] != trg:
+        trg = prec[trg]
+        L.append(trg)
+    L.append(trg) # trg is the source
     return L
